bf1c6c1223352cdada2daab1719b8a4aa5471023
Gritt Zheng <Gritt Zheng>
2019-11-25 10:31:52 +0800

    给tableView添加了datasource

--- GTD/GTD/ChecklistViewController.swift
+++ GTD/GTD/ChecklistViewController.swift
@@ -16,5 +16,17 @@ class checkListViewController: UITableViewController {
     }
 
 
+    
+    //MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    override func  tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 }
+
+
+
 
