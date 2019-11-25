acd7aa2ecfa4a14d608be06adb598feb12c329da
Gritt Zheng <Gritt Zheng>
2019-11-25 14:22:17 +0800

    给table添加数据

--- GTD/GTD/ChecklistViewController.swift
+++ GTD/GTD/ChecklistViewController.swift
@@ -19,14 +19,24 @@ class checkListViewController: UITableViewController {
     
     //MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        5
     }
     override func  tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        
+        if indexPath.row == 0 {
+            label.text = "观看《流浪地球》"
+        }else if indexPath.row == 1 {
+            label.text = "观看《疯狂的外星人》"
+        }else if indexPath.row == 2 {
+            label.text = "观看《飞驰人生》"
+        }else if indexPath.row == 3 {
+            label.text = "观看《喜剧之王》"
+        }else if indexPath.row == 4 {
+            label.text = "观看《小猪佩奇过大年》"
+        }
         
         
         
